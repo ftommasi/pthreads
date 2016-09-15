d2b1fe415d568e891024773dda931bc21b89c434
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-09-14 19:07:53 -0500

    changes

--- pthread.c
+++ pthread.c
@@ -21,19 +21,20 @@ void* thread_entry(void* args){
 }
 
 int main(int argc, char** argv){
+  unsigned bufSize = 100;
   int n = atoi(argv[1]);
   struct thread_entry_args arg_array[n];
+  pthread_t threads[n];
   int i =0;
-  for(i; i<n; i++){
+  for( i = 0; i < n; i++ ){
     pthread_t id;
     struct thread_entry_args args;
     args.arg1 = 10;
     args.arg2 = (char*)malloc(246*sizeof(char));
-  
-    strcpy(args.arg2,"Chris and Fausto\0");
-  
-    printf("MAIN: arg1 %d arg2 %s\n",args.arg1,args.arg2);
-    int create_error= pthread_create(&id,NULL,thread_entry,&args);
+    arg_array[i] = args;
+    threads[i] = id;
+    snprintf(arg_array[i].arg2, bufSize, "This is thread %d", i);
+    int create_error= pthread_create(threads[i],NULL,thread_entry,&args);
     if(create_error != 0){
       printf("ERROR IN CREATE\n") ;
     }
@@ -42,8 +43,10 @@ int main(int argc, char** argv){
     if(join_error!=0){
       printf("ERROR IN JOIN\n");
     }
-    printf("ret is %d in main\n",args.ret);
-    free(args.arg2);
+
   }
+    //printf("MAIN: arg1 %d arg2 %s\n",args.arg1,args.arg2);
+     //  printf("ret is %d in main\n",args.ret);
+    //free(args.arg2);
   return 0;
 }
